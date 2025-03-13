2e9bf36c20437e7df3878a6669a055b501b01e7b
El Mehdi Kenane <elmehdi.kenane@gmail.com>
2025-03-13 17:33:40 +0000

    try to fix middleware issue

--- middleware.ts
+++ middleware.ts
@@ -8,22 +8,26 @@ export async function middleware(req: NextRequest) {
   }
   console.log("================ 1 ================");
   console.log("req.nextUrl.origin", req.nextUrl.origin);
-  const validateRes = await fetch(
-    `${req.nextUrl.origin}/api/auth/validate-session`,
-    {
-      headers: { Cookie: `auth_session=${sessionId}` },
-      cache: "no-store",
-    },
-  );
-  console.log("================ 2 ================");
-
-  const { authenticated } = await validateRes.json();
-  console.log("================ 3 ================");
-
-  if (!authenticated) {
-    const response = NextResponse.redirect(new URL("/auth/sign-in", req.url));
-    response.cookies.delete("auth_session");
-    return response;
+  try {
+    const validateRes = await fetch(
+      `${req.nextUrl.origin}/api/auth/validate-session`,
+      {
+        headers: { Cookie: `auth_session=${sessionId}` },
+        cache: "no-store",
+      },
+    );
+    console.log("================ 2 ================");
+    console.log("validateRes status:", validateRes.status);
+    console.log("validateRes headers:", validateRes.headers);
+    const { authenticated } = await validateRes.json();
+    console.log("================ 3 ================");
+    if (!authenticated) {
+      const response = NextResponse.redirect(new URL("/auth/sign-in", req.url));
+      response.cookies.delete("auth_session");
+      return response;
+    }
+  } catch (error) {
+    console.error("Fetch error in middleware:", error);
   }
   console.log("================ 4 ================");
 
